0d5b6d89547457c439439207f55dac3618076f19
Kaoru Mori <kaoru@nagoya.local>
2016-12-31 08:47:32 +0900

    Add access_token accessor

--- lib/fitbit/client.rb
+++ lib/fitbit/client.rb
@@ -1,11 +1,14 @@
 module Fitbit
   class Client
+    attr_accessor :access_token
+
     # Returns a new instance of Client
     # @param [String] client_id: OAuth2 client id.
     # @param [String] client_secret: OAuth2 client secret.
-    # @param [String] access_token: OAuth2 access token.
+    # @param [String] token: OAuth2 access token.
     # @param [String] refresh_token: OAuth2 refresh token.
-    def initialize(client_id:, client_secret:, access_token:, refresh_token:, expires_at:)
+    # @param [FixNum, String] expires_at: OAuth2 expires at.
+    def initialize(client_id:, client_secret:, token:, refresh_token:, expires_at:)
       @basic_token = Base64.strict_encode64("#{client_id}:#{client_secret}")
 
       @oauth2_client = OAuth2::Client.new(client_id, client_secret,
@@ -14,15 +17,7 @@ module Fitbit
 
       opts = { refresh_token: refresh_token,
                expires_at: expires_at }
-      @access_token = OAuth2::AccessToken.new(@oauth2_client, access_token, opts)
-    end
-
-    def tokens
-      return {
-        access_token: @access_token.token,
-        refresh_token: @access_token.refresh_token,
-        expires_at: @access_token.expires_at
-      }
+      @access_token = OAuth2::AccessToken.new(@oauth2_client, token, opts)
     end
 
     def refresh!

0f1eb5bac60041fb1adf63f3028da2f387c0ee2a
Mark Kreyman <mark@kreyman.com>
2017-02-08 19:43:50 -0700

    Add list_subscriptions endpoint

--- lib/fitbit/subscription.rb
+++ lib/fitbit/subscription.rb
@@ -18,6 +18,19 @@ module Fitbit
       end
     end
 
+    # The List Subscriptions endpoint retrieves a list of a user's subscriptions.
+    # @param [String] user_id: The encoded ID of the user. Use "-" (dash) for current logged-in user.
+    # @param [String] collection_path: Collection to get subscriptions for (foods, activities, sleep, or body).
+    #                                  If not present, list will include all subscriptions for the user.
+    # @return [Hash] response data from Fitbit API.
+    def list_subscriptions(user_id: '-', collection_path: nil)
+      if collection_path
+        return get("#{API_URI}/user/#{user_id}/#{collection_path}/apiSubscriptions.json")
+      else
+        return get("#{API_URI}/user/#{user_id}/apiSubscriptions.json")
+      end
+    end
+
     # The Delete Subscription endpoint updates the app to stop sending notifications when the
     # user Fitbit data changes. The endpoint response includes a summary of the subscription
     # @param [String] user_id: The encoded ID of the user. Use "-" (dash) for current logged-in user.

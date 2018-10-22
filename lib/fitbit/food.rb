03725f2438e0b006cd14e94240a90a11e2754d37
Korey Enright <kenright19@gmail.com>
2018-10-22 17:01:43 -0500

    Add food time series

--- lib/fitbit/food.rb
+++ lib/fitbit/food.rb
@@ -12,5 +12,29 @@ module Fitbit
     def food_logs(user_id: '-', date: Date.today)
       return get("#{API_URI}/#{FOOD_API_VERSION}/user/#{user_id}/foods/log/date/#{date}.json")
     end
+
+    # given resource in the format requested using units in the
+    # {https://dev.fitbit.com/docs/basics/#units unit system} that corresponds to the
+    # Accept-Language header provided.
+    # @overload sleep_time_series(date:, period:)
+    #   @param [String] user_id: The encoded ID of the user. Use "-" (dash) for current logged-in user.
+    #   @param [String] resource_path: The resource path; see the Resource Path Options below for a list of options.
+    #   @param [String] date: The end date of the period specified in the format yyyy-MM-dd or today.
+    #   @param [String] period: The range for which data will be returned. Options are 1d, 7d, 30d, 1w, 1m, 3m, 6m, 1y, or max.
+    # @overload sleep_time_series(base_date:, end_date:)
+    #   @param [String] user_id: The encoded ID of the user. Use "-" (dash) for current logged-in user.
+    #   @param [String] resource_path: The resource path; see the Resource Path Options below for a list of options.
+    #   @param [String] base_date: The range start date, in the format yyyy-MM-dd or today.
+    #   @param [String] end_date: The end date of the range.
+    # @return [Hash] response data from Fitbit API
+    def food_time_series(user_id: '-', resource_path:, date: nil, period: nil, base_date: nil, end_date: nil)
+      if date and period
+        return get("#{API_URI}/#{FOOD_API_VERSION}/user/#{user_id}/#{resource_path}/date/#{date}/#{period}.json")
+      elsif base_date and end_date
+        return get("#{API_URI}/#{FOOD_API_VERSION}/user/#{user_id}/#{resource_path}/date/#{base_date}/#{end_date}.json")
+      else
+        raise StandardError
+      end
+    end
   end
 end
